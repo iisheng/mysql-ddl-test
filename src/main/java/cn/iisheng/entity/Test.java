4fd839049c25607186c59bdc7e5702150cfcf35a
iisheng <opensheng@gmail.com>
2020-05-18 09:53:27 +0800

    Init project

---
+++ src/main/java/cn/iisheng/entity/Test.java
@@ -0,0 +1,9 @@
+package cn.iisheng.entity;
+
+/**
+ * @author iisheng
+ * @date 2020/05/18 09:50:00
+ */
+public class Test {
+    private Long id;
+}
---
+++ src/test/java/cn/iisheng/task/TestTask.java
@@ -0,0 +1,9 @@
+package cn.iisheng.task;
+
+/**
+ * @author iisheng
+ * @date 2020/05/18 09:50:50
+ */
+public class TestTask {
+
+}
